6b7c4913d72c655a450e31314d077197a75af6ac
rururufus <rururufus@ya.ru>
2025-12-13 16:52:38 +0300

    fix postgres user

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    USER = os.environ.get('POSTGRES_USER', 'postgres_user')
+    USER = os.environ.get('POSTGRES_USER', 'postgres')
     PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'password')
     HOST = os.environ.get('POSTGRES_HOST', 'postgres')
     PORT = os.environ.get('POSTGRES_PORT', '5432')
