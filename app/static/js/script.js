cff189a60aacb65258a3981c053e972b2c2118b1
Ru Fus <retub-88-09-01@mail.ru>
2026-04-05 20:51:17 +0300

    js test again

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const heart = document.querySelector('.heart-outline');
-    
+
     if (!heart) return;
+
+    const startAnimation = () => {
+    heart.classList.add('animate');
+  };
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 // Запускаем анимацию, добавляя класс
-                heart.classList.add('animate');
+                startAnimation();
                 // Отключаем наблюдение, чтобы не перезапускать
                 observer.unobserve(entry.target);
             }
@@ -17,5 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
         rootMargin: '0px 0px -100px 0px' // Небольшой запас снизу
     });
 
-    observer.observe(heart);
+    const isSafari = () => {
+        const ua = navigator.userAgent;
+        // Safari имеет WebKit, но не Chrome, не iOS WebView, не сторонние браузеры
+        return /Safari/.test(ua) &&
+            /Apple Computer/.test(navigator.vendor) &&
+            !/Chrome|CriOS|FxiOS|EdgiOS|OPR/.test(ua);
+    };
+
+    // Использование:
+    if (isSafari()) {
+        setTimeout(startAnimation, 5000);
+    } else {
+        observer.observe(heart);
+    }
 });
